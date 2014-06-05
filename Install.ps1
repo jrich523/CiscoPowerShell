7f2dadd4cef92259bd91dd3c73f11eebb965e15d
Justin <jrich523@gmail.com>
2014-06-05 13:59:58 -0700

    Installer and Docs added

---
+++ Install.ps1
@@ -0,0 +1,41 @@
+﻿param([string]$InstallDirectory)
+$ModName = "CiscoPowerShell"
+$repo = "CiscoPowerShell"
+$userid = "jrich523"
+
+if ('' -eq $InstallDirectory)
+{
+    $personalModules = Join-Path -Path ([Environment]::GetFolderPath('MyDocuments')) -ChildPath WindowsPowerShell\Modules
+    if (($env:PSModulePath -split ';') -notcontains $personalModules)
+    {
+        Write-Warning "$personalModules is not in `$env:PSModulePath"
+    }
+
+    if (!(Test-Path $personalModules))
+    {
+        Write-Error "$personalModules does not exist"
+    }
+
+    $InstallDirectory = Join-Path -Path $personalModules -ChildPath $ModName
+}
+if (!(Test-Path $InstallDirectory))
+{
+    $null = mkdir $InstallDirectory    
+}
+
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile("https://raw.github.com/$userid/$repo/master/$modname.psd1","$installDirectory\$modname.psd1")
+Push-Location
+cd $InstallDirectory
+(Import-LocalizedData -FileName "$ModName.psd1").filelist | %{$wc.DownloadFile("https://raw.github.com/$userid/$repo/master/$_","$installDirectory\$_")}
+gci | Unblock-File
+Pop-Location
+
+
+
+<#
+Run to generate install cmd
+
+write-host "iex (new-object System.Net.WebClient).DownloadString('https://raw.github.com/$userid/$repo/master/Install.ps1')"
+
+#>
---
+++ README.md
@@ -0,0 +1,12 @@
+Cisco PowerShell Module
+=======================
+
+connects in to a cisco unit to gather and parse data.
+
+IOS - 6.0(2)N1(2)
+
+PowerShell - created/tested on V5, should be V3 compatible
+
+# Install #
+
+	iex (new-object System.Net.WebClient).DownloadString('https://raw.github.com/jrich523/CiscoPowerShell/master/Install.ps1')
